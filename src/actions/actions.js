b8f70aa5106202cc824251a1b28aacee18d0b357
isaac stott <isaac.cloyd@gmail.com>
2020-05-12 16:33:18 -0600

    redux hates me

---
+++ src/actions/actions.js
@@ -0,0 +1,23 @@
+// Action Suite
+// 1. action types
+// 2. action creators
+// 3. actions (action objects)
+
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+// action creators are just functions that "create" (aka return) actions
+export const removeFeature = () => {
+  console.log("action creator called");
+  return { type: REMOVE_FEATURE };
+};
+
+export const ADD_FEATURE = "ADD_FEATURE";
+
+export const addFeature = newFeature => {
+  return { type: ADD_FEATURE, 
+    payload: {
+        additionalPrice: newFeature.price,
+        newFeature: newFeature
+    }
+  };
+};
+
--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { removeFeature } from "../actions/actions";
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={props.removeFeature} className="button">X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  console.log({ state });
+  return {
+    removeF: state.removeF
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { removeFeature }
+)(AddedFeature);
